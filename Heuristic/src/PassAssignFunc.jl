3ff9fe84acc1c02b0ec4349fcb46cf2e57f54289
sillewib <s214712@dtu.dk>
2026-06-01 09:42:53 +0200

    stochastic

--- Heuristic/src/PassAssignFunc.jl
+++ Heuristic/src/PassAssignFunc.jl
@@ -308,6 +308,9 @@ function assign_passengersV2(evtols::allPlaneSolution, data, rt::Matrix{Int})
     price_by_group = Dict(a => q[a] * fd[(op[a], dp[a])] * (so[a] == 1 ? 0.75 : 1.0) for a in A)
 =======
     price_by_group = Dict(a => fd[(op[a], dp[a])] * (so[a] == 1 ? 0.75 : 1.0) for a in A)
+<<<<<<< HEAD
+>>>>>>> d33dc32 (stochastic)
+=======
 >>>>>>> d33dc32 (stochastic)
     # Sort groups directly by price (descending)
     prices = [price_by_group[a] for a in A]
